ea549281c870fc8610468b4adfe4e436914c48e4
Jhonni10 <jhonieripianoscue@gmail.com>
2023-06-06 22:32:38 -0500

    creacion de carrusel de imagenes

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Rutas from './routes/PublicRutas';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App h-screen">
       <Rutas/>
     </div>
   );
--- src/routes/PublicRutas.jsx
+++ src/routes/PublicRutas.jsx
@@ -1,12 +1,26 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Index from '../pages/Index';
+import Products from '../pages/Products';
+import Navbar from '../components/Navbar';
+import Locations from '../pages/Locations';
+import Employees from '../pages/Employees';
+import Jobs from '../pages/Jobs';
+import Contact from '../pages/Contact';
+import About from '../pages/About';
 
 const Rutas = () => {
     return (
         <BrowserRouter>
+            <Navbar/>
             <Routes>
                 <Route path='/' element={<Index/>}/>
+                <Route path='/productos' element={<Products/>}/>
+                <Route path='/ubicaciones' element={<Locations/>}/>
+                <Route path='/empleados' element={<Employees/>}/>
+                <Route path='/trabajos' element={<Jobs/>}/>
+                <Route path='/contactanos' element={<Contact/>}/>
+                <Route path='/quienes-somos' element={<About/>}/>
             </Routes>
         </BrowserRouter>
     );
