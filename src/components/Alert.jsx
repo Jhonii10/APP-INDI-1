7489b3fcc481817e63844bfa4d4a92a4740dd506
Jhonni10 <jhonieripianoscue@gmail.com>
2023-09-09 23:24:08 -0500

    New login and page of register

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -2,9 +2,29 @@ import React from 'react';
 
 const Alert = ({massage}) => {
     return (
-        <div className='bg-red-100 border-red-400 text-red-700 px-4 py-3'>
-            <span className='sm:inline block'>{massage}</span>
+        <div role="alert" className="rounded-xl border-s-4 border-red-100 bg-red-100 p-4">
+        <div className="flex items-center gap-4">
+          <span className="text-red-600">
+          <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="currentColor"
+      class="h-5 w-5"
+    >
+      <path
+        fill-rule="evenodd"
+        d="M9.401 3.003c1.155-2 4.043-2 5.197 0l7.355 12.748c1.154 2-.29 4.5-2.599 4.5H4.645c-2.309 0-3.752-2.5-2.598-4.5L9.4 3.003zM12 8.25a.75.75 0 01.75.75v3.75a.75.75 0 01-1.5 0V9a.75.75 0 01.75-.75zm0 8.25a.75.75 0 100-1.5.75.75 0 000 1.5z"
+        clip-rule="evenodd"
+      />
+    </svg>
+          </span>
+          <div className="flex-1">
+            <p className="mt-1 text-sm text-gray-700">
+              {massage}
+            </p>
+          </div>
         </div>
+      </div>
     );
 }
 
