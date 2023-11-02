9ec9a4686881061858cdd717125cb1868e6dd847
Jhonni10 <jhonieripianoscue@gmail.com>
2023-11-02 11:45:45 -0500

    new components AppStatistics v.1.2

--- src/app/components/secctions/@dashboard/app/AppStatistics.js
+++ src/app/components/secctions/@dashboard/app/AppStatistics.js
@@ -1,13 +1,73 @@
 import { Box, Card, Stack, Typography } from '@mui/material';
 import React from 'react';
+import ReactApexChart from 'react-apexcharts';
+import SvgColor from '../../../svg-colors/SvgColor';
 
-const AppStatistics = ({porcentaje, title , valores}) => {
+const AppStatistics = ({porcentaje, title , valores, charDate, colorColumn}) => {
+
+    const validacion = parseFloat(porcentaje)
+    console.log(validacion);
+
+    
+    const chartOptions = {
+        chart: {
+          type: 'bar',
+          width: 100,
+          height: 36,
+          sparkline: {
+            enabled: true,
+          },
+        },
+        plotOptions: {
+          bar: {
+            columnWidth: '80%',
+            borderRadius: 2,
+            colors: {
+              ranges: [
+                {
+                  from: 0,
+                  to: 100,
+                  color: colorColumn,
+                },
+              ],
+            },
+          },
+        },
+        labels: Array.from({ length: charDate.length }, (_, i) => i + 1),
+        xaxis: {
+          crosshairs: {
+            width: 1,
+          },
+        },
+        tooltip: {
+          fixed: {
+            enabled: false,
+          },
+          x: {
+            show: false,
+          },
+          y: {
+            title: {
+              formatter: () => '',
+            },
+          },
+          marker: {
+            show: false,
+          },
+        },
+      };
+    
+      const icon = (name) => <SvgColor src={`/assets/icons/accesories/${name}.svg`} sx={{ width: '24px', height: '24px', marginRight:'8px',...(parseFloat(porcentaje) > 0.0 ?{color:'rgb(34, 197, 94)'}:{color:'rgb(255, 86, 48)'}) }}/>;
+    
+
     return (
         <>
             <Card sx={{
                         '& *':{ fontFamily:'"Quicksand", sans-serif'},
                         padding:'24px',
-                        display:'flex'
+                        display:'flex',
+                        alignItems:'center',
+                        textAlign:'left'
                         }} >
                 <Box sx={{
                     flexGrow:'1'
@@ -27,31 +87,8 @@ const AppStatistics = ({porcentaje, title , valores}) => {
                         alignItems: 'center',
                         marginTop:'16px',
                         marginBottom:'8px',
-                        '& svg':{ width:'24px', height:'24px', marginRight:'8px',color:'rgb(34, 197, 94)'}
                     }}>
-                    {/* SVG */}
-                    <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    xmlnsXlink="http://www.w3.org/1999/xlink"
-                    aria-hidden="true"
-                    role="img"
-                    className="component-iconify MuiBox-root css-v0h3dx iconify iconify--solar"
-                    width="1em"
-                    height="1em"
-                    viewBox="0 0 24 24"
-                    >
-                    <g fill="currentColor">
-                        <path
-                        d="M5 17.75a.75.75 0 0 1-.488-1.32l7-6a.75.75 0 0 1 .976 0l7 6A.75.75 0 0 1 19 17.75H5Z"
-                        opacity=".5"
-                        />
-                        <path
-                        fillRule="evenodd"
-                        d="M4.43 13.488a.75.75 0 0 0 1.058.081L12 7.988l6.512 5.581a.75.75 0 1 0 .976-1.138l-7-6a.75.75 0 0 0-.976 0l-7 6a.75.75 0 0 0-.081 1.057Z"
-                        clipRule="evenodd"
-                        />
-                    </g>
-                    </svg>
+                    { parseFloat(porcentaje) > 0.0 ? icon('ic_portcent_positive') : icon('ic_portcent_negative')}
 
                     <Typography component={'div'} variant='subtitle2' sx={{
                         fontWeight:'600',
@@ -72,9 +109,17 @@ const AppStatistics = ({porcentaje, title , valores}) => {
 
                 </Box>
 
+                <Box dir="ltr">
+                    <ReactApexChart options={chartOptions} series={[{ data: charDate }]} type="bar" height={36} width={60} />
+                </Box>
+
             </Card>
         </>
     );
 }
 
 export default AppStatistics;
+
+
+
+
