f9209750bdbd2bc07beea60e22f69323c514190b
Jhonni10 <jhonieripianoscue@gmail.com>
2023-12-04 12:06:33 -0500

    new page userpage

--- src/app/routes/routes.js
+++ src/app/routes/routes.js
@@ -2,6 +2,7 @@ import { Navigate, useRoutes } from 'react-router-dom';
 import AppLayout from '../layout/AppLayout';
 import CubikaApp from '../pages/CubikaApp';
 import DashboardApp from '../pages/DashboardApp';
+import UserPage from '../pages/UserPage';
 
 // ----------------------------------------------------------------------
 
@@ -13,8 +14,8 @@ export default function Router() {
       children: [
         { element: <Navigate to="/dashboard/app" replace />, index: true },
         { path: 'app', element: <DashboardApp/> },
-        { path: 'commerce', element: <CubikaApp title={'commerce'} /> },
-        { path: 'user', element: <CubikaApp title={'user'} /> },
+        { path: 'commerce', element: <CubikaApp title={'cubika'} /> },
+        { path: 'user', element: <UserPage /> },
         { path: 'products', element: <CubikaApp title={'productos'} /> },
         { path: 'blog', element: <CubikaApp title={'blog'} /> },
         { path: 'analytics', element: <CubikaApp title={'analytics'} /> },
